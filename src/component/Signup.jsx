7496c640bedfe01fbb33b385833bf84cf7ce4de1
hardikrana929 <hardikrana929@gmail.com>
2026-06-02 16:40:45 +0530

    change signup url

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -49,7 +49,7 @@ const Signup = () => {
       return;
     }
     try {
-      const res = await axios.post("http://localhost:3000/api/student/signup-student", {
+      const res = await axios.post("https://eduvault-backend.vercel.app/api/student/signup-student", {
         ...formData,
       });
 
