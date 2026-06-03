2c622f03f30e3abee1e8fec0b50826dfc376146c
hardikrana929 <hardikrana929@gmail.com>
2026-06-03 14:22:25 +0530

    change url

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
         });
       } else {
         await axios
-          .post("eduvalut-backend.vercel.app/api/student/login-student", {
+          .post("https://eduvault-backend.vercel.app/api/student/login-student", {
             ...formData,
           })
           .then((res) => {
