ab5e95de3b068d3b5ae372e0d7c041e0d07a8ec3
Asavala Hardik <hardikrana929@gmail.com>
2026-06-21 10:37:30 +0530

    change protectedRouter

--- src/component/ProtectedRouter.jsx
+++ src/component/ProtectedRouter.jsx
@@ -1,27 +1,58 @@
+// import { Navigate } from "react-router-dom";
+
+// export const ProtectedRouter = ({ children, role }) => {
+//   const isUser = JSON.parse(localStorage.getItem("user"));
+//   if (!isUser) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   // role check
+//   if (role && isUser.role !== role) {
+//     return (
+//       <Navigate
+//         to={isUser.role === "admin" ? "/adminDash" : "/stdDash"}
+//         replace
+//       />
+//     );
+//   }
+//   return children;
+// };
+
+// export const AuthAccess = ({ children }) => {
+//   const isUser = JSON.parse(localStorage.getItem("user"));
+//   if (!isUser) {
+//     return <Navigate to="/" replace />;
+//   }
+//   return children;
+// };
 import { Navigate } from "react-router-dom";
 
-export const ProtectedRouter = ({ children, role }) => {
-  const isUser = JSON.parse(localStorage.getItem("user"));
-  if (!isUser) {
+export const ProtectedRouter = ({
+  children,
+  role,
+}) => {
+  const token = localStorage.getItem("token");
+
+  const user = JSON.parse(
+    localStorage.getItem("user")
+  );
+
+  if (!token || !user) {
     return <Navigate to="/login" replace />;
   }
 
-  // role check
-  if (role && isUser.role !== role) {
+  if (role && user.role !== role) {
     return (
       <Navigate
-        to={isUser.role === "admin" ? "/adminDash" : "/stdDash"}
+        to={
+          user.role === "admin"
+            ? "/adminDash"
+            : "/stdDash"
+        }
         replace
       />
     );
   }
-  return children;
-};
 
-export const AuthAccess = ({ children }) => {
-  const isUser = JSON.parse(localStorage.getItem("user"));
-  if (!isUser) {
-    return <Navigate to="/" replace />;
-  }
   return children;
 };
